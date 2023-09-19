71a9a23a2772cd423fd02144f32d4caf67b5f463
Asror <asrorrustmaovich007@gmail.com>
2023-09-19 18:14:13 +0500

    sdcsd

--- ISM.Infrastructure/Services/ServiceForOrders.cs
+++ ISM.Infrastructure/Services/ServiceForOrders.cs
@@ -4,7 +4,7 @@ using ISM.Infrastructure.Repositories;
 
 namespace ISM.Infrastructure.Services
 {
-    internal class ServiceForOrders
+    internal class ServiceForOrders:IOrderService
     {
         private IOrderRepository _repository;
         public ServiceForOrders() => _repository = new RepositoriyForOrders();
